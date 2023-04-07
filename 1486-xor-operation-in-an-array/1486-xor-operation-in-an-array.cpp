963e5a210335d5131a1c9ccb494f06751a42055c
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-04-07 08:57:40 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (73.51%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int xorOperation(int n, int start) {
-        int arr[n];
+        int arr[n],res = 0;
         for(int i = 0 ; i < n ; i++){
-            arr[i] = start + 2 *i;
+            arr[i] = start + 2 *i;\
+                res ^= arr[i];
         }
-        int res = arr[0];
-        for(int i = 1 ; i < n ; i++){
-            res = res^arr[i];
-        }
+
         return res;
         
     }
