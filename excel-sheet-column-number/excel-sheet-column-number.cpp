9c06f49737030cc1ae27fb934b515bfeecd54c89
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-04-29 22:44:12 +0530

    Time: 7 ms (19.54%) | Memory: 5.9 MB (94.84%) - LeetSync

---
+++ excel-sheet-column-number/excel-sheet-column-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int res = 0 ;
+        for(auto i : columnTitle){
+            res = res*26+(i-'A'+1);
+        }
+        return res;
+    }
+};
