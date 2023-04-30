b0d4cf1fef675e4472e6751571761766e7093c68
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-04-30 12:09:59 +0530

    Time: 28 ms (90.57%) | Memory: 16.2 MB (8.26%) - LeetSync

--- excel-sheet-column-number/excel-sheet-column-number.py
+++ excel-sheet-column-number/excel-sheet-column-number.py
@@ -1,6 +1,6 @@
 class Solution:
-    def titleToNumber(self, columnTitle: str) -> int:
-        ans = 0 
-        for i in columnTitle:
-            ans = ans * 26 + ord(i)-ord('A')+1
-        return ans 
+    def titleToNumber(self, n: str) -> int:
+        res = 0 
+        for i in n :
+            res = res*26+(ord(i)-ord('A')+1)
+        return res
