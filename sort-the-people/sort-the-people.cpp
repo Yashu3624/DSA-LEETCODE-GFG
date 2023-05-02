681f8d78a10afc7d827b535725ca32f19f4cfbf6
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-05-03 01:27:58 +0530

    Time: 44 ms (34.49%) | Memory: 22.8 MB (68.38%) - LeetSync

--- sort-the-people/sort-the-people.cpp
+++ sort-the-people/sort-the-people.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        for(int i = 0 ; i < names.size()-1 ;i++){
-
-            for(int j = i+1 ; j<names.size() ; j++){
-                if(heights[i]<heights[j]){
-                    swap(names[i],names[j]);
-                    swap(heights[i],heights[j]);
-                }
-            }
-        }
-        return names;
+        
+         vector<string>ans;
+         unordered_map<int,string>mp ;
+         for(int i = 0 ; i < names.size() ; i++){
+                mp[heights[i]]=names[i];
+         }
+         sort(heights.rbegin() , heights.rend());
+         for(int i = 0 ; i < names.size() ; i++){
+             ans.push_back(mp[heights[i]]);
+         }
+return ans;
     }
 };
