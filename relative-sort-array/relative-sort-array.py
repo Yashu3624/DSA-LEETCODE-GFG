4935c147d29696228e0f9058b5b9875a82b07bc0
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-05-04 14:55:52 +0530

    Time: 55 ms (18.01%) | Memory: 16.5 MB (5.21%) - LeetSync

--- relative-sort-array/relative-sort-array.py
+++ relative-sort-array/relative-sort-array.py
@@ -1,7 +1,7 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         res = []
-        l = []
+        rem = []
         for i in arr2:
 
             s = arr1.count(i)
@@ -9,7 +9,7 @@ class Solution:
                 res.append(i)
         for i in arr1:
             if i not in arr2:
-                l.append(i)
-        l = sorted(l)
-        res += l
+                rem.append(i)
+        rem = sorted(rem)
+        res += rem
         return res
