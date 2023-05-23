c613f59a4b6572993476cd53050b3ec037c198e1
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-05-23 08:48:36 +0530

    Added solution - LeetHub

--- Remove Consecutive Characters - GFG/remove-consecutive-characters.py
+++ Remove Consecutive Characters - GFG/remove-consecutive-characters.py
@@ -1,17 +1,16 @@
 #User function Template for python3
 
 class Solution:
-    def removeConsecutiveCharacter(self, S):
-        
+    def removeConsecutiveCharacter(self, s):
         # code here
-        res = []
-        for i in S:
-            if len(res)>0 and i==res[-1]:
-                res.pop()
-            res.append(i)
-        return ''.join(res)
-        
-        
+        n = len(s)
+        ans = ""
+        ans += s[0]
+        for i in range(1,n):
+            if s[i]!=s[i-1]:
+                ans += s[i]
+        return ans
+
 
 #{ 
  # Driver Code Starts
