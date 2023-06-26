a6903d39a07b91e37f174454e020f00dcd74b2b5
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-06-26 17:48:04 +0530

    Time: 3 ms (50.50%) | Memory: 40.2 MB (98.88%) - LeetSync

---
+++ count-asterisks/count-asterisks.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countAsterisks(String s) {
+        int res = 0 , bar = 0 ; 
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s.charAt(i)=='*' && bar%2==0)
+            res++ ;
+            if(s.charAt(i)=='|')
+            bar++;
+        }
+        return res ;
+    }
+}
