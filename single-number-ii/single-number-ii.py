cc7a3b37e1351c617e71ac0f497c0e23184e4ad8
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-07-04 16:12:24 +0530

    Time: 79 ms (50.61%) | Memory: 18.5 MB (6.77%) - LeetSync

--- single-number-ii/single-number-ii.py
+++ single-number-ii/single-number-ii.py
@@ -1,6 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        nums = Counter(nums)
-        for ele , fre in nums.items():
-            if fre == 1:
+        res = defaultdict(int)
+        
+        for num in nums:
+            res[num] += 1
+        for ele , freq in res.items():
+            if freq == 1:
                 return ele
+        return -1
