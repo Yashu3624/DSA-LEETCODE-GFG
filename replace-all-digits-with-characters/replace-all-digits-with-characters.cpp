33cf8ccd6bad88054988bbc045c1d41d1b45ce3e
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-07-13 15:04:19 +0530

    Time: 4 ms (40.05%) | Memory: 6 MB (77.36%) - LeetSync

--- replace-all-digits-with-characters/replace-all-digits-with-characters.cpp
+++ replace-all-digits-with-characters/replace-all-digits-with-characters.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     string replaceDigits(string s) {
-        for(int i = 1  ; i < s.size();i+=2){
-            s[i]=s[i]+s[i-1]-'0';
+        for(int i  = 0 ; i < s.size() ; i++){
+            if(i%2==1){
+                s[i] = s[i]+s[i-1]-'0';
+            }
         }
         return s;
     }
