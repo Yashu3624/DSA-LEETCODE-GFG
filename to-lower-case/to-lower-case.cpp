4e9ffd99307df506233fd9ac5ca5659f1d4bbc4d
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-07-13 15:00:18 +0530

    Time: 0 ms (100.00%) | Memory: 6.1 MB (84.29%) - LeetSync

--- to-lower-case/to-lower-case.cpp
+++ to-lower-case/to-lower-case.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     string toLowerCase(string s) {
         for(int i = 0 ; i < s.size() ; i++){
-            if(s[i]>=65 && s[i]<=90)
+            if(s[i]>=65 && s[i]<=90){
                 s[i] += 32;
+            }
         }
         return s;
-        
     }
 };
