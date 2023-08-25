eba89d1384e5b42d32eb3042defd7de391159b53
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-08-25 22:37:13 +0530

    Time: 64 ms (59.68%) | Memory: 34.4 MB (29.54%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size() ;
+        int  m = mat[0].size() ;
+        
+         vector<vector<int>> vis(n, vector<int>(m,0));
+        vector<vector<int>> distance(n, vector<int>(m,0));
+
+        queue<pair<pair<int,int>,int>> q;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ;j < m ; j++){
+                if(mat[i][j]==0){
+                    q.push({{i,j},0});
+                    vis[i][j] =1;
+                }
+            }
+        }
+        while(!q.empty()){
+            int r = q.front().first.first ;
+            int c = q.front().first.second ;
+            int t = q.front().second ;
+            q.pop();
+
+            int delRow[] = {-1,0,1,0};
+            int delCol[] ={0,1,0,-1};
+            for(int i = 0 ; i < 4 ; i++){
+                int nrow = r+delRow[i];
+                int ncol = c+delCol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && mat[nrow][ncol]==1){
+                    vis[nrow][ncol] = 1 ;
+                    q.push({{nrow,ncol},t+1});
+                    distance[nrow][ncol] = 1+t ;
+                }
+
+            }
+        }
+
+        return distance;
+    }
+};
