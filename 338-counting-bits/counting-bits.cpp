12b962a18cf4c0d218030bf41976177bf9da9d2a
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-09-01 22:40:35 +0530

    Time: 8 ms (29.62%) | Memory: 8.5 MB (14.67%) - LeetSync

---
+++ 338-counting-bits/counting-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>res ;
+        for(int i = 0 ; i <= n ; i++){
+            int cnt = 0 ; 
+            int num = i ;
+            while(num){
+                cnt += num%2;
+                num =num>>1;
+            }
+            res.push_back(cnt);
+        }
+        return res;
+    }
+};
