2fe62b3af4c5bf3160b248821e6d809ca93aea99
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-09-03 11:26:56 +0530

    Time: 3 ms (45.28%) | Memory: 6.5 MB (56.80%) - LeetSync

---
+++ 1944-truncate-sentence/truncate-sentence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        string ans = "";
+        int cnt = 0 ;
+        for(int i = 0 ; i < s.size() ; i++){
+            if(s[i]==' ')
+                cnt++;
+            if(cnt==k) 
+                break;
+            ans += s[i];
+        }
+        return ans;
+    }
+};
