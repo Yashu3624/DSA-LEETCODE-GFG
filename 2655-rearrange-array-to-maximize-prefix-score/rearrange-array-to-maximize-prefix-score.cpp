6f34f03c777a8786a79b4c21a5c1b988cecd254e
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-09-05 13:56:03 +0530

    Time: 127 ms (75.00%) | Memory: 83.1 MB (40.16%) - LeetSync

--- 2655-rearrange-array-to-maximize-prefix-score/rearrange-array-to-maximize-prefix-score.cpp
+++ 2655-rearrange-array-to-maximize-prefix-score/rearrange-array-to-maximize-prefix-score.cpp
@@ -4,8 +4,8 @@ public:
         long long sum = 0 ; 
         int res = 0 ;
         sort(nums.begin() , nums.end() , greater<int>());
-        for(auto it : nums){
-            sum += it;
+        for(int i = 0 ; i < nums.size() ; i++ ){
+            sum += nums[i];
             if(sum>0) res++;
         }
         return res;
