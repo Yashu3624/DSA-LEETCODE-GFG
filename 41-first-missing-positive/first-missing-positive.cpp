f780eeb24b732e322e997059068dec93eb418bc7
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-09-15 21:48:39 +0530

    Time: 52 ms (59.27%) | Memory: 43.1 MB (22.71%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int firstMissingPositive(vector<int>& nums) {
-        set<int>st ; 
-        int n = nums.size();
-        for(int i = 0 ; i < n ; i++){
-            st.insert(nums[i]);
+    int firstMissingPositive(vector<int>& A) {
+        int n = A.size();
+    vector<int>hash(n+1,0);
+    for(int i=0;i<n;i++){
+        if(A[i]<=n && A[i]>0){
+            hash[A[i]]++;
         }
-        int j = 1; 
-        while(j<=n){
-            if(st.find(j)!=st.end()) j++;
-            else
-            return j ;
+    }
+    for(int i=1;i<=n;i++){
+        if(hash[i]==0){
+            return i;
         }
-        return j ; 
+    }
+    return hash.size();
     }
 };
