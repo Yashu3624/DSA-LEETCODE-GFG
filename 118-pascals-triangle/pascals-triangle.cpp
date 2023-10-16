c4d893579fcf1ea66b569617f42e54a0f8c32f74
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-10-16 19:22:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (10.76%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,21 +1,21 @@
 class Solution {
-    vector<int>generateRow(int row){
-        long long ans = 1 ;
-        vector<int>Row ;
-        Row.push_back(1);
-        for(int col = 1 ; col<row;col++){
-            ans = ans *(row-col);
-            ans = ans/col;
-            Row.push_back(ans);
-        } 
-        return Row;
-    }
 public:
-    vector<vector<int>> generate(int r) {
-        vector<vector<int>>ans;
-        for(int i = 1 ; i <= r ; i++ ){
-            ans.push_back(generateRow(i));
+    vector<int>generateRow(int row ){
+        vector<int>ans ; 
+        long long a = 1 ; 
+        ans.push_back(1);
+        for(int i = 1 ; i < row ; i++){
+            a = a*(row-i);
+            a = a/(i) ; 
+            ans.push_back(a);
         }
         return ans;
+    }
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res ;
+        for(int i = 1 ; i <= numRows ; i++){
+            res.push_back(generateRow(i));
+        }
+        return res;
     }
 };
