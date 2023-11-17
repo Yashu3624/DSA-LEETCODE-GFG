afea50d2c9bae7c147eb7561244397280de20713
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-11-17 07:09:51 +0530

    Time: 6 ms (44.79%) | Memory: 8.3 MB (6.62%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string ans  ="";
-        for(int i = 0 ; i < s.size() ; i++ ){
-           if(isalnum(s[i])){
-               ans += tolower(s[i]);
-           } 
-        }
-        int i = 0 ;
-        int j = ans.size()-1;
-        while(i<j){
-            if(ans[i++]!=ans[j--]){
-                return false;
+        string ans = "";
+        for(int i = 0 ; i < s.size() ; i++){
+            if(isalnum(s[i])){
+                ans += tolower(s[i]);
             }
         }
-        return true;
-        
+        string res =ans;
+         reverse(ans.begin(),ans.end());
+        return res==ans;
     }
 };
