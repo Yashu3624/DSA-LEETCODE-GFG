b36a0ecb17a8daf28384136896a1d808444bf9c7
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-12-05 17:04:46 +0530

    Time: 11 ms (23.84%) | Memory: 10.3 MB (71.27%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -5,12 +5,11 @@ public:
         int m = grid[0].size() ; 
         vector<vector<int>>dp(n,vector<int>(m,0));
         dp[0][0] = grid[0][0];
-        for(int i = 1 ; i < m ; i++){
-            dp[0][i] = dp[0][i-1]+grid[0][i];
+        for(int i = 1 ; i < m || i< n ; i++){
+            if(i<m)dp[0][i] = dp[0][i-1]+grid[0][i];
+            if(i<n)dp[i][0] = dp[i-1][0]+grid[i][0];
         }
-        for(int j = 1 ; j < n ; j++){
-            dp[j][0] = dp[j-1][0]+grid[j][0];
-        }
+
         for(int i = 1 ; i < n ; i++){
             for(int j = 1 ; j < m ; j++){
                 int up = grid[i][j]+dp[i-1][j];
