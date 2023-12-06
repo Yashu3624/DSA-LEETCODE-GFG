a2ec713bafe992c419c2ad1b9698f88c6166b7a1
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-12-07 00:12:11 +0530

    Time: 3 ms (13.68%) | Memory: 6.9 MB (6.05%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -3,18 +3,18 @@ public:
     int totalMoney(int n) {
         vector<int>dp(n,-1);
         dp[0] = 1;
+        int sum = 1 ;
         for(int i = 1 ; i < n ; i++){
             if(i<=6){
                 dp[i] = dp[i-1]+1;
+                sum += dp[i];
             }
             else{
                 dp[i] = dp[i-7]+1;
+                sum += dp[i];
             }
         }
-        int sum = 0 ;
-        for(int i = 0 ; i < n ; i++){
-            sum += dp[i];
-        }
+     
         return sum;
            
         }
