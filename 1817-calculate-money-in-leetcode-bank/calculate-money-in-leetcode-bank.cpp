1ad9204a823a1a2bbb8be01b23464a3529012eef
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-12-07 00:08:34 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (6.05%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int m=n/7;  //(no.of full weeks)
-        // first week  1 2 3 4 5 6 7 (sum is 28 i.e. 7*(i+3) if i=1)
-        // second week  2 3 4 5 6 7 8 (sum is 35 i.e. 7*(i+3) if i=2)
-        //.... so on
-        int res=0; //for result
-        //calculating full weeks
-        for(int i=1;i<=m;i++){
-            res+=7*(i+3);
+        vector<int>dp(n,-1);
+        dp[0] = 1;
+        for(int i = 1 ; i < n ; i++){
+            if(i<=6){
+                dp[i] = dp[i-1]+1;
+            }
+            else{
+                dp[i] = dp[i-7]+1;
+            }
         }
-        //calculating left days
-        for(int i=7*m;i<n;i++){
-            res+=++m;
+        int sum = 0 ;
+        for(int i = 0 ; i < n ; i++){
+            sum += dp[i];
         }
-        return res;
-    }
+        return sum;
+           
+        }
+    
 };
