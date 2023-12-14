2aba58fb4996491cc6450718e1ee6ed719613e44
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-12-14 14:29:02 +0530

    Time: 16 ms (55.34%) | Memory: 26.6 MB (50.28%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -13,20 +13,23 @@ class Solution {
 public:
     map<int,int>mp;
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    
         for(int i = 0 ; i < inorder.size() ; i++){
             mp[inorder[i]] = i ;
         }
         TreeNode* root = build(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1);
-        return root;
+        return root ; 
     }
-    TreeNode* build(vector<int>&preorder,int preStart , int preEnd , vector<int>&inorder,int inStart , int inEnd){
-        if(preStart>preEnd || inStart>inEnd) return NULL;
-        TreeNode* root = new TreeNode(preorder[preStart]);
+    TreeNode* build(vector<int>&preorder,int preStart , int preEnd,vector<int>&inorder,int inStart , int inEnd){
+        if(preStart>preEnd || inStart >inEnd) return NULL ; 
+        TreeNode*root = new TreeNode(preorder[preStart]);
         int index = mp[preorder[preStart]];
-        int len = index - inStart;
-        root->left = build(preorder , preStart+1 , preStart+len , inorder,inStart,index-1);
-        root->right = build(preorder,preStart+len+1 ,preEnd , inorder,index+1,inEnd);
-        return root;
-
+        int len = index-inStart ;
+        root->left = build(preorder,preStart+1,preStart+len , inorder,inStart , index-1);
+        root->right = build(preorder,preStart+len+1,preEnd,inorder,index+1,inEnd);
+        return root; 
     }
+ 
+
+    
 };
