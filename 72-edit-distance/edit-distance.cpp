58f7c15d98ba7d81e84b4b5d131415fb28e92228
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-12-20 21:56:44 +0530

    Time: 7 ms (83.80%) | Memory: 9.3 MB (46.37%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-    int f(int i , int j  , string s , string t,vector<vector<int>>&dp){
-        if(i<0) return j+1 ; 
-        if(j<0) return i+1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j]){
-            return 0 + f(i-1,j-1,s,t,dp);
-        }
-        return dp[i][j] = min(1+f(i,j-1,s,t,dp),min(1+f(i-1,j,s,t,dp),1+f(i-1,j-1,s,t,dp)));
-    }
     int minDistance(string s, string t) {
         int n = s.size() ; 
         int m = t.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return f(n-1,m-1,s,t,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i = 1 ; i <= m ; i++){
+            dp[0][i] = i ;
+        }
+        for(int i = 1 ; i <= n ; i++){
+            dp[i][0] = i ;
+        }
+        for(int i = 1 ; i <= n ; i++){
+            for(int j =1 ; j <= m ; j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 1 + min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
