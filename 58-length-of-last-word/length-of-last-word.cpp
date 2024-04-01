83db1ceb097c53d26feacbc13ac0029caad3d3b2
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2024-04-01 21:33:59 +0530

    Time: 4 ms (47.87%) | Memory: 7.7 MB (57.50%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-            istringstream iss(s);
-
-     string lastword ; 
-     while(iss>>lastword) {
-
-     } 
-     return lastword.size();  
+        int count = 0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]==' ' && count==0)continue;
+            if(s[i]==' ' && count!=0)return count;
+            if(s[i]!=' ')count++;
+        }
+        return count;
     }
 };
