3253000c1d4b5075d69ca0aeb0153f5fbe44e604
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2024-04-12 14:04:55 +0530

    Time: 13 ms (35.99%) | Memory: 22.4 MB (46.83%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int trap(vector<int>& arr) {
-        int n = arr.size();
-        int left[n], right[n];
-        int water = 0 ; 
-        left[0] = arr[0];
-        right[n-1] = arr[n-1];
-        
+    int trap(vector<int>& height) {
+        int n = height.size() ; 
+        int left[n];
+        int right[n];
+        left[0] = height[0];
+        right[n-1] = height[n-1];
+        int res = 0 ;
         for(int i = 1 ; i < n ; i++){
-            left[i] = max(left[i-1],arr[i]);
+            left[i] = max(left[i-1],height[i]);
         }
-        for(int i = n-2 ; i >=0 ; i--){
-            right[i] = max(right[i+1],arr[i]);
+        for(int i = n-2 ; i >= 0 ; i--){
+            right[i] = max(right[i+1],height[i]);
         }
-        for(int i = 1 ; i < n - 1 ; i++){
-            int var = min(left[i-1],right[i+1]);
-            if(var>arr[i]){
-                water += var-arr[i];
+        for(int i =  1 ; i < n-1 ; i++){
+            int temp = min(left[i-1],right[i+1]);
+            if(temp>height[i]){
+                res += temp-height[i];
             }
         }
-        return water ;
+        return res;
     }
-    
 };
