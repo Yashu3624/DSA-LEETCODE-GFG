42e22ee286a2fa830121f90366a48be94b14950a
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2024-04-15 10:37:55 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (96.99%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -11,11 +11,12 @@
  */
 class Solution {
 public:
-    int getSum(TreeNode* root , int curr){
-        if(root==NULL) return 0 ;
-        curr = (curr*10+root->val);
-        if(root->left==NULL && root->right==NULL) return curr;
-        return getSum(root->left,curr)+getSum(root->right,curr);
+    int getSum(TreeNode* root , int sum){
+        if(root==NULL) return 0 ; 
+        sum = root->val+sum*10 ; 
+        if(root->left==NULL && root->right==NULL) return sum ; 
+        return getSum(root->left,sum)+getSum(root->right,sum);
+
     }
     int sumNumbers(TreeNode* root) {
         return getSum(root,0);
