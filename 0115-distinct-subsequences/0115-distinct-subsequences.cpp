3cb4e88e69ad7217b19a7affe281dc638d1928ad
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2024-11-06 18:27:53 +0530

    Time: 2857 ms (5.82%), Space: 421.3 MB (10.79%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    int solve(int i , int j , int n , int m , string s , string t){
+    int solve(int i , int j , int n , int m , string s , string t,vector<vector<int>>&dp){
         if(j==m) return 1 ;
         if(i==n) return 0 ; 
+        if(dp[i][j]!=-1) return dp[i][j] ;
         if(s[i]==t[j]){
-            return solve(i+1,j+1 , n , m , s , t) + solve(i+1,j,n,m,s,t);
+            return dp[i][j] = solve(i+1,j+1 , n , m , s , t,dp) + solve(i+1,j,n,m,s,t,dp);
         }
         else{
-            return solve(i+1 , j , n , m , s , t) ;
+            return dp[i][j] = solve(i+1 , j , n , m , s , t,dp) ;
         }
     }
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size() ; 
-        return solve(0 , 0 , n , m , s , t ) ; 
+        vector<vector<int>>dp(n,vector<int>(m,-1)) ;
+        return solve(0 , 0 , n , m , s , t ,dp) ; 
     }
 };
