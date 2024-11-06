27264efb788e92f97be93d7387ad0846308a04af
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2024-11-06 15:17:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    int change(int amount, vector<int>& coins) {
-        int n = coins.size()  ;
-        vector<vector<int>>dp(n,vector<int>(amount+1,0));
-        for(int i = 0 ;  i <n ; i++){
-            dp[i][0] = 1 ; 
-        }
-        for(int i = 0 ; i <= amount ; i++){
-            if(i%coins[0]==0) dp[0][i] = 1;
-        }
-        for(int i = 1 ; i < n ; i++){
-            for(int j = 1 ; j <= amount ; j++){
-                if(coins[i]>j)
-                dp[i][j] = dp[i-1][j];
-                else
-                dp[i][j] = dp[i-1][j]+dp[i][j-coins[i]];
+    int solve(int idx , vector<int>& coins , int amount,vector<vector<int>>&dp){
+        if(idx==coins.size()-1){
+            if(amount%coins[idx]==0){
+                return 1;
             }
+            return 0 ; 
+           
         }
-        return dp[n-1][amount];
+        if(dp[idx][amount]!=-1) return dp[idx][amount] ;
+        int notPick = 0 + solve(idx+1 , coins , amount,dp) ; 
+        int pick = 0 ; 
+        if(coins[idx]<= amount){
+            if(solve(idx , coins , amount-coins[idx],dp)!=0)
+            pick =  solve(idx , coins , amount-coins[idx],dp) ;
+        }
+        return pick+notPick ;
+    }
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size() ;
+        vector<vector<int>>dp(n , vector<int>(amount+1 , -1)) ;
+        return solve(0 , coins , amount,dp) ;
     }
 };
