175d1dc00491bdc8ef16b8c4e3bceffdea0b2260
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2024-11-06 19:06:37 +0530

    Time: 1162 ms (7.11%), Space: 781.1 MB (5.14%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-    int f(int i, int j, string& s, string& p, vector<vector<int>>& dp) {
-        if (i == 0 && j == 0) return 1;
-        if (j == 0) return 0;
-        if (i == 0) {
-            for (int jj = 1; jj <= j; ++jj) {
-                if (p[jj - 1] != '*') return 0;
-            }
-            return 1;
-        }
-        if (dp[i][j] != -1) return dp[i][j];
 
-        if (s[i - 1] == p[j - 1] || p[j - 1] == '?') {
-            return dp[i][j] = f(i - 1, j - 1, s, p, dp);
-        }
-        if (p[j - 1] == '*') {
 
-            return dp[i][j] = (f(i, j - 1, s, p, dp) || f(i - 1, j, s, p, dp)) ? 1 : 0;
+    int solve(int i , int j , string s , string p , int n , int m , vector<vector<int>>&dp){
+        if(i==n && j==m) return 1 ;
+        if(j==m) return 0 ;
+        if(dp[i][j]!=-1) return dp[i][j] ;
+    if (p[j] == '*') {
+            return dp[i][j] = solve(i, j + 1, s, p,n,m,dp) || (i < s.size() && solve(i + 1, j, s, p,n,m,dp));
         }
+if(i < s.size() && (p[j] == '?' || s[i] == p[j])) return dp[i][j] =  solve(i + 1, j + 1, s, p, n, m,dp);
+        return 0 ;
+        
 
-        return dp[i][j] = 0;
     }
 
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        return f(n, m, s, p, dp) == 1;
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1)) ;
+        return solve(0 , 0 , s , p , n , m , dp ) ; 
+
     }
 };
