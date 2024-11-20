35a304b6b6890a37ae2cb1536f6637b5d686f91b
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2024-11-20 07:28:23 +0530

    Time: 10 ms (73.17%), Space: 11.9 MB (96.89%) - LeetHub

--- 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
@@ -1,37 +1,35 @@
-
 class Solution {
 public:
     int takeCharacters(string s, int k) {
-        int cntA = 0, cntB = 0, cntC = 0;
-        int n = s.size();
-        
-        for (char c : s) {
-            if (c == 'a') cntA++;
-            if (c == 'b') cntB++;
-            if (c == 'c') cntC++;
+        int cntA  = 0 , cntB = 0 , cntC = 0  ; 
+        int n = s.size() ; 
+    
+        for(int i = 0 ; i < n ; i++){
+            if(s[i]=='a') cntA += 1 ; 
+            if(s[i]=='b') cntB += 1 ; 
+            if(s[i]=='c') cntC += 1 ; 
         }
+            if(cntA<k || cntB<k || cntC <k) return -1 ; 
+            int remA = cntA- k ; 
+            int remB = cntB- k ; 
+            int remC = cntC- k ; 
+            cntA = cntB = cntC = 0 ; 
+            int ans = 0 , j = 0 ; 
+            for(int i = 0 ; i < n ; i++){
+                cntA += (s[i]=='a') ; 
+                cntB += (s[i]=='b') ; 
+                cntC += (s[i]=='c') ; 
+               while((cntA > remA || cntB > remB || cntC > remC)){
 
-        if (cntA < k || cntB < k || cntC < k) return -1;
-
-        int remA = cntA - k, remB = cntB - k, remC = cntC - k;
-        int maxLen = 0, j = 0;
-        cntA = cntB = cntC = 0;
-
-        for (int i = 0; i < n; i++) {
-            cntA += (s[i] == 'a');
-            cntB += (s[i] == 'b');
-            cntC += (s[i] == 'c');
-
-            while (cntA > remA || cntB > remB || cntC > remC) {
-                cntA -= (s[j] == 'a');
-                cntB -= (s[j] == 'b');
-                cntC -= (s[j] == 'c');
-                j++;
+                    cntA -= (s[j]=='a') ; 
+                    cntB -= (s[j]=='b') ; 
+                    cntC -= (s[j]=='c') ;
+                    j++ ;
+                }
+                ans = max(ans , i-j+1)  ;
             }
 
-            maxLen = max(maxLen, i - j + 1);
-        }
-
-        return n - maxLen;
+        
+        return n-ans ; 
     }
 };
