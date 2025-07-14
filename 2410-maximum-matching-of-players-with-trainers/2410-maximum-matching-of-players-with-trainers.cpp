c053bd0ee32e582d04d66ebf7e7916265c145e9c
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2025-07-14 18:27:01 +0530

    Time: 30 ms (48.79%), Space: 80.2 MB (60.25%) - LeetHub

--- 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.cpp
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.cpp
@@ -3,18 +3,16 @@ public:
     int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
         int n = players.size() ; 
         int m = trainers.size() ; 
-        sort(players.begin(), players.end()) ;
-        sort(trainers.begin() , trainers.end()) ; 
-        int cnt = 0 ; 
-        int i = 0 , j = 0  ; 
+        sort(begin(players) , end(players)) ;
+        sort(begin(trainers) , end(trainers)) ;
+        int i = 0 , j = 0 , cnt = 0 ; 
         while(i<n && j < m){
-            if(players[i]<=trainers[j]){
+            if(players[i]<= trainers[j]){
+                i++ ; 
                 cnt++ ; 
-                i++ ;
-                
             }
-            j++ ;
+            j++ ; 
         }
-        return cnt ; 
+        return cnt ;
     }
 };
