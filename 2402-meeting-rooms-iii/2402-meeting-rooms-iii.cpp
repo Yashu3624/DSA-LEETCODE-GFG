7753bff2e021749c6d20344632807228f9711bad
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2025-07-15 18:47:50 +0530

    Time: 132 ms (10.38%), Space: 111.3 MB (9.32%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
@@ -1,46 +1,45 @@
 class Solution {
 public:
     int mostBooked(int n, vector<vector<int>>& meetings) {
-        vector<long long>lastAvailabelAt(n,0) ; 
-        int m = meetings.size() ; 
-        sort(begin(meetings),end(meetings)) ; 
-        bool found = false ;
-
         vector<int>roomUsedCnt(n,0) ; 
-        for(vector<int>&meet : meetings){
+        int m = meetings.size(); 
+        sort(meetings.begin() , meetings.end()) ; 
+        vector<long long > lastAvailable(n,0) ;
+        for(vector<int>meet: meetings){
             bool found = false ;
             int start = meet[0] ; 
-            int end = meet[1] ;
-            int duration = end-start ;
-long long earlyEndRoomTime = LLONG_MAX;
+            int end = meet[1] ; 
+            int duration = end - start ;
+            long long earlyEndRoomTime = LLONG_MAX ; 
             int earlyEndRoom = 0 ;
-            for(int room =0 ;room< n ; room++){
-                if(lastAvailabelAt[room]<=start){
-                    lastAvailabelAt[room] = end ; 
-                    roomUsedCnt[room]++ ;
-                    found = true;
+
+            for(int room = 0 ; room < n ; room++){
+                if(lastAvailable[room] <= start){
+                    lastAvailable[room] = end ; 
+                    roomUsedCnt[room]++ ; 
+                    found = true ;
                     break;
                 }
-                if(lastAvailabelAt[room] < earlyEndRoomTime){
-                    earlyEndRoomTime = lastAvailabelAt[room] ; 
-                    earlyEndRoom = room ; 
+                if(lastAvailable[room]<earlyEndRoomTime){
+                    earlyEndRoomTime = lastAvailable[room] ; 
+                    earlyEndRoom  = room ; 
                 }
 
             }
-            if(!found){
-                lastAvailabelAt[earlyEndRoom] += duration ; 
-                roomUsedCnt[earlyEndRoom]++;
-
-            }
+                            if(!found ){
+                    lastAvailable[earlyEndRoom] += duration ;
+                    roomUsedCnt[earlyEndRoom]++ ;  
+                }
         }
-        int result = -1  ;
-        int maxUse = 0 ; 
-        for(int room = 0 ; room<n ; room++){
+        int result = -1 ; 
+        int maxUse = 0 ;
+        for(int room = 0 ; room < n ; room++){
             if(roomUsedCnt[room]>maxUse){
                 maxUse = roomUsedCnt[room] ; 
-                result = room ; 
+                result = room ;
             }
+
         }
-        return result ; 
+        return result ;
     }
 };
