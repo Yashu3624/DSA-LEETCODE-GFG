e74be1b45a9dc6aaf367eb75fb0fda5dd4d64113
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2025-07-29 15:42:59 +0530

    Time: 139 ms (14.88%), Space: 310.1 MB (11.33%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    void findMaxOrSubSets(int idx , int res , vector<int>&nums , int sum , int& cnt ){
-        if(idx>=nums.size()){
+    int findMaxOrSubSets(int idx , int res , vector<int>&nums , int sum , vector<vector<int>>&dp ){
+        if(idx==nums.size()){
         if(res==sum){
-            cnt++ ;
+            return 1 ;
         }
-        return ;
+        return  0 ;
         }
-        findMaxOrSubSets(idx+1,res|nums[idx],nums , sum , cnt) ;
-        findMaxOrSubSets(idx+1,res,nums,sum,cnt) ;
+        int pick = findMaxOrSubSets(idx+1,res|nums[idx],nums , sum , dp) ;
+        int  notPick = findMaxOrSubSets(idx+1,res,nums,sum,dp) ;
+        return dp[idx][res] = pick+notPick ; 
     }
     int countMaxOrSubsets(vector<int>& nums) {
         int sum = 0 ; 
+        int n = nums.size();
         int cnt = 0 ; 
         for(int i : nums){
             sum |= i ;
         }
-        findMaxOrSubSets(0,0,nums,sum,cnt) ;
+        vector<vector<int>>dp(n,vector<int>(sum+1,-1)) ;
+         cnt = findMaxOrSubSets(0,0,nums,sum,dp) ;
         return cnt ;
     }
 };
