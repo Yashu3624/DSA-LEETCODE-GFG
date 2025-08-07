7668a586493fe145c6b55d29a52ca85a9acdf6d2
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2025-08-07 14:34:19 +0530

    Time: 80 ms (61.79%), Space: 189.1 MB (47.17%) - LeetHub

--- 3479-fruits-into-baskets-iii/3479-fruits-into-baskets-iii.cpp
+++ 3479-fruits-into-baskets-iii/3479-fruits-into-baskets-iii.cpp
@@ -1,38 +1,48 @@
 class Solution {
-public:
-    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
-        int n = baskets.size();
-        int N = 1;
-        while (N <= n) N <<= 1;
-
-        vector<int> segTree(N << 1);
-        for (int i = 0; i < n; ++i)
-            segTree[N + i] = baskets[i];
+public: 
+    void build(int i , int l , int r , vector<int>&baskets , vector<int>&segmentTree){
+        if(l==r){
+            segmentTree[i] = baskets[l] ;
+            return ;
+        }
+        int mid = l+(r-l)/2 ; 
+        build(2*i+1,l,mid,baskets,segmentTree) ; 
+        build(2*i+2,mid+1,r,baskets,segmentTree) ; 
+        segmentTree[i] = max(segmentTree[2*i+1],segmentTree[2*i+2]) ; 
         
-        for (int i = N - 1; i > 0; --i)
-            segTree[i] = max(segTree[2 * i], segTree[2 * i + 1]);
+    }
+    bool querySegmentTree(int i , int l , int r , vector<int>&segmentTree , int fruit){
+        if(segmentTree[i]<fruit){
+            return false ;
+        }
+        if(l==r){
+            segmentTree[i] = -1 ;
+            return true ;
+        }
+        int mid = l+(r-l)/2 ; 
+        bool placed = false ; 
+        if(segmentTree[2*i+1]>=fruit){
+            placed = querySegmentTree(2*i+1,l,mid , segmentTree,fruit) ;
 
-        int count = 0;
-        for (int fruit : fruits) {
-            int index = 1;
-            if (segTree[index] < fruit) {
-                count++;
-                continue;
-            }
+        }
+        else{
+            placed = querySegmentTree(2*i+2,mid+1,r , segmentTree,fruit) ;
 
-            while (index < N) {
-                if (segTree[2 * index] >= fruit)
-                    index = 2 * index;
-                else
-                    index = 2 * index + 1;
-            }
-
-            segTree[index] = -1;
-            while (index > 1) {
-                index >>= 1;
-                segTree[index] = max(segTree[2 * index], segTree[2 * index + 1]);
+        }
+        segmentTree[i] = max(segmentTree[2*i+1],segmentTree[2*i+2]) ; 
+        return placed ; 
+    }
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int n = fruits.size() ; 
+        vector<int>segmentTree(4*n,-1) ; 
+        build(0,0,n-1,baskets,segmentTree) ;
+        int unplaced = 0 ; 
+        for(int& fruit : fruits){
+            if(querySegmentTree(0,0,n-1,segmentTree,fruit)==false){
+                unplaced++ ;
             }
         }
-        return count;
+        return unplaced ; 
+
     }
 };
