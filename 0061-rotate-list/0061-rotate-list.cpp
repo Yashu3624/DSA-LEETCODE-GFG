52be0a976368cfc6973e14a937601073943238a6
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2026-05-05 22:16:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -12,21 +12,20 @@ class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
         if(head==NULL || head->next==NULL) return head ; 
-        int cnt = 1 ;
-        ListNode* temp = head;
+        int cnt = 1 ; 
+        ListNode* temp = head ; 
         while(temp->next!=NULL){
             temp = temp->next ; 
             cnt++ ;
         }
-        temp->next = head ;
         k = k % cnt ; 
-        int rem = cnt- k ;
+        temp->next = head ; 
+        int rem = cnt - k ;
         for(int i = 0 ; i < rem ; i++){
-            temp = temp->next ;
-
+            temp = temp->next ; 
         }
         head = temp->next ; 
-        temp->next = NULL ;
-        return head;
+        temp->next = NULL ; 
+        return head ; 
     }
 };
