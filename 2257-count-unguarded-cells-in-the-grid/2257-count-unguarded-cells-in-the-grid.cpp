90c3c3f6ddb535dffa7d3e84805b7fb60760716f
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2026-08-08 17:37:49 +0530

    Time: 55 ms (77.02%), Space: 173.1 MB (61.8%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
-    void solve(int i, int j, vector<vector<int>>& dp, int m, int n, int di, int dj){
-      if( i < 0 || i >= m || j < 0 || j >= n ||dp[i][j]==1|| dp[i][j]==2){
-            return  ;
-      } 
-      dp[i][j]  = 3 ; 
-      solve(i+di,j+dj,dp,m,n,di,dj) ;
-   
-    }
     int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
-        vector<vector<int>>dp(m,vector<int>(n,0)) ; 
-        for(auto& guard : guards){
-            dp[guard[0]][guard[1]] = 1 ; 
+        vector<vector<int>>grid(m,vector<int>(n,0)) ; 
+        int X[] = {-1,0,1,0} ; 
+        int Y[] = {0,-1,0,1} ; 
+        for(auto&g:guards){
+            grid[g[0]][g[1]] = 1 ; 
         }
-        for(auto& wall: walls ){
-            dp[wall[0]][wall[1]] = 2 ; 
+        for(auto&w:walls){
+            grid[w[0]][w[1]] = 2 ; 
         }
-        for(int i = 0 ; i < guards.size() ; i++){
-            solve(guards[i][0]-1 , guards[i][1],dp,m,n,-1,0  ) ; 
-            solve(guards[i][0]+1 , guards[i][1],dp,m,n,1,0  )  ;
-            solve(guards[i][0] , guards[i][1]-1,dp,m,n,0,-1  ) ;
-            solve(guards[i][0] , guards[i][1]+1,dp,m,n,0,1  ) ;
+        for(auto&g :guards){
+            int x = g[0] ; 
+            int y = g[1] ; 
+            for(int i = 0 ; i <  4 ; i++){
+                int dx = x+X[i] ; 
+                int dy = y+Y[i] ; 
+                while(dx>=0 && dx<m &&dy>=0 && dy<n && grid[dx][dy]!=2 &&  grid[dx][dy]!=1){
+                     grid[dx][dy] = 3 ;
+                     dx += X[i] ; 
+                     dy += Y[i] ; 
+                }
+            }
         }
-        int cnt = 0 ; 
-        for(int i = 0 ; i  < m ; i++){
+        int cnt = 0  ;
+        for(int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
-                if(dp[i][j]==0)
-                cnt++ ; 
+                if(grid[i][j]==0){
+                    cnt++ ; 
+                }
             }
         }
         return cnt ; 
-        
-
-
     }
 };
