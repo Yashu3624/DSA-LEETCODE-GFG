49c727d8fdc6a0741e339d99a03fe27e4d7178c2
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2026-08-08 18:14:39 +0530

    Time: 0 ms (100%), Space: 12 MB (83.15%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
-    void dfs(int i , int j ,vector<vector<char>>& board,vector<vector<int>>&vis ){
-        int n = board.size() ; 
-        int m = board[0].size() ;
-        if(i>=n || j >=m) return ;
-        if(board[i][j]=='.') return ; 
-        vis[i][j] = 1 ;
-        dfs(i,j+1,board,vis) ; 
-        dfs(i+1,j,board,vis) ; 
-    }
+    
     int countBattleships(vector<vector<char>>& board) {
         int n = board.size() ; 
         int m = board[0].size() ; 
         int cnt = 0  ; 
-        vector<vector<int>>vis(n,vector<int>(m,0)) ; 
+      
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
-                if(!vis[i][j] && board[i][j]=='X' ){
-                    dfs(i,j,board,vis) ; 
-                    cnt++ ; 
-                }
+                if(board[i][j]=='X'){
+             if(i>0 && board[i-1][j]=='X') continue ;
+             if(j>0 && board[i][j-1]=='X') continue ; 
+             cnt++ ;
+            }
             }
         }
         return cnt ; 
